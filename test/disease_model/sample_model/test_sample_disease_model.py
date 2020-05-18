1b7aff06ed4a697acb077d4bbb450580fc3b5d41
shub <shub@auquan.com>
2020-05-18 16:00:03 +0530

    Minor fixes

--- test/disease_model/sample_model/test_sample_disease_model.py
+++ test/disease_model/sample_model/test_sample_disease_model.py
@@ -1,9 +1,7 @@
+""" Test for sample diease model """
+
 import pytest # pylint: disable=unused-import
 
 def test_sample_passing_method():
     """ Passing Test for a sample function """
     assert True
-
-def test_sample_failing_method():
-    """ Failing Test for a sample function """
-    assert False
