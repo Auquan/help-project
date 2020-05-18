b0adf7e20084b83487144a3ded4d0d89926c1828
shub <shub@auquan.com>
2020-05-18 15:36:39 +0530

    Add ci config with test

---
+++ test/disease_model/sample_model/test_sample_disease_model.py
@@ -0,0 +1,9 @@
+import pytest # pylint: disable=unused-import
+
+def test_sample_passing_method():
+    """ Passing Test for a sample function """
+    assert True
+
+def test_sample_failing_method():
+    """ Failing Test for a sample function """
+    assert False
