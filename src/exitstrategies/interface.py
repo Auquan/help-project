30a9f3aa8e382364d54ad5455937735b06bff0cd
whymath <20425672+whymath@users.noreply.github.com>
2020-06-08 22:34:40 +0530

    1) V0 code for exit strategies module (updated output as dictionary of dataframes), 2) pylint - Suppressed W0640 (Cell variable defined in loop)

--- src/exitstrategies/interface.py
+++ src/exitstrategies/interface.py
@@ -2,25 +2,34 @@
 Interface class to be used by other modules
 """
 
-from help_project.src.exitstrategies.elt.dataextractor import DataExtractor
+from help_project.src.exitstrategies.data_elt import DataELT
 
 
 class ExitStrategies():
-    focus_areas = ["agriculture", "chemical", "commerce", "construction", "education", "fin_prof_services",
-                   "food_consumables", "healthcare", "hospitality_tourism", "manufacturing", "mining", "engineering",
-                   "media", "energy", "telecom", "public_admin", "supply_chain_ship", "forest_husb_fish", "textiles",
-                   "transportation", "utilities", "gathering_size", "open_border", "air_travel", "roal_rail_travel",
-                   "public_transport", "curfew", "ecommerce", "contact_tracing", "covid_testing"]
+    # TODO: Update these
+    focus_areas_eco = ["agriculture", "chemical", "commerce", "construction", "education", "fin_prof_services",
+                       "food_consumables", "healthcare", "hospitality_tourism", "manufacturing", "mining",
+                       "engineering", "media", "energy", "telecom", "public_admin", "supply_chain_ship",
+                       "forest_husb_fish", "textiles", "transportation", "utilities"]
+    # TODO: Update these
+    focus_areas_soc = ["gathering_size", "open_border", "air_travel", "roal_rail_travel", "public_transport", "curfew",
+                       "ecommerce", "contact_tracing", "covid_testing"]
 
     def __init__(self):
         pass
 
-    def get_lockdown_vectors(self):
-        # Fetch list of lockdown vectors
-        data_extractor = DataExtractor()
-        lockdown_vector = data_extractor.transform_data()
-
-        return lockdown_vector
+    def get_exit_strategies(self):
+        data_elt = DataELT()
+        exit_strategies = data_elt.extract_data()
+        # TODO: parameterize start_date, end_date, start_day (numerical day since day 1), end_day, format (df/array/map)
+        return exit_strategies
 
     def get_focus_areas(self):
-        return self.focus_areas
+        focus_areas = self.focus_areas_eco.extend(self.focus_areas_soc)
+        return focus_areas
+
+    def get_focus_areas_eco(self):
+        return self.focus_areas_eco
+
+    def get_focus_areas_soc(self):
+        return self.focus_areas_soc
---
+++ test/exitstrategies/test_interface.py
@@ -0,0 +1,36 @@
+"""
+Test for dummy lockdown vector
+"""
+
+from os.path import dirname, join
+from help_project.src.exitstrategies.interface import ExitStrategies
+
+
+def test_strat_dict():
+    pass_test = False
+    print_output_files = False
+
+    # Generate strategies dictionary
+    exit_strat = ExitStrategies()
+    strat_dict = exit_strat.get_exit_strategies()
+
+    for strat_id in strat_dict:
+        # Check for zero rows or columns
+        strat_df = strat_dict[strat_id]
+        if (strat_dict[strat_id].shape[0] == 0) or (strat_dict[strat_id].shape[1] == 0):
+            pass_test = False
+            break
+
+        # Print to file if required
+        if print_output_files:
+            folder_name = dirname(dirname(__file__))
+            folder_name = join(folder_name, "exitstrategies")
+            file_name = strat_id + "_output.csv"
+            file_path = join(folder_name, file_name)
+            print("\n Printing output to:", file_path)
+            strat_df.to_csv(file_path)
+
+        # Set flag to pass test
+        pass_test = True
+
+    assert pass_test
